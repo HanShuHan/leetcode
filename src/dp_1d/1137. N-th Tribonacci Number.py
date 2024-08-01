26daf0ad143811677d55c870a79c58575f7334cf
Shu-Han Han <dragon1988k@gmail.com>
2024-08-02 07:58:55 +0800

    add 1137. N-th Tribonacci Number solution 2

--- src/dp_1d/1137. N-th Tribonacci Number.py
+++ src/dp_1d/1137. N-th Tribonacci Number.py
@@ -9,3 +9,28 @@ class Solution:
         for _ in range(n - 2):
             n0, n1, n2 = n1, n2, (n0 + n1 + n2)
         return n2
+
+
+class Solution2:
+
+    def tribonacci(self, n: int) -> int:
+        if n <= 1:
+            return n
+        elif n == 2:
+            return 1
+
+        mapping = {
+            0: 0,
+            1: 1,
+            2: 1
+        }
+
+        def tribonacciNth(n: int) -> int:
+            if n in mapping:
+                return mapping[n]
+            else:
+                result = tribonacciNth(n - 3) + tribonacciNth(n - 2) + tribonacciNth(n - 1)
+                mapping[n] = result
+                return result
+
+        return tribonacciNth(n)
