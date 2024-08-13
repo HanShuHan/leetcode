e629171246c2422b1a79f37d91d22d6c49fa447e
Shu-Han Han <dragon1988k@gmail.com>
2024-08-13 22:10:01 +0800

    add 62. Unique Paths

--- src/main.py
+++ src/main.py
@@ -1,6 +1,25 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if (m, n) == (0, 0):
+            return 0
+
+        pathsCount = 0
+        curPosOfPaths = deque([[0, 0]])
+
+        while curPosOfPaths:
+            pos = curPosOfPaths.popleft()
+
+            if pos == [m - 1, n - 1]:
+                pathsCount += 1
+            if pos[0] < m:
+                curPosOfPaths.append([pos[0] + 1, pos[1]])
+            if pos[1] < n:
+                curPosOfPaths.append([pos[0], pos[1] + 1])
+
+        return pathsCount
+
+
 if __name__ == '__main__':
     # Start from here
-    first3 = (0, 1, 1)
-    n0, n1, n2 = first3
-    n0, n1, n2 = 2, 3, 4
-    print(n0, n1, n2)
+    dp = [[1] * 3 for _ in range(10)]
+    print(dp)
