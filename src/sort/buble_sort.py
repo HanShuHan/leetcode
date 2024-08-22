dbe9090a44dfa472c7d591a0911a80af1ae4cf4d
Shu-Han Han <dragon1988k@gmail.com>
2024-08-22 22:29:33 +0800

    add buble sort

---
+++ src/sort/buble_sort.py
@@ -0,0 +1,10 @@
+def buble_sort(arr: list[int]) -> list[int]:
+    if len(arr) <= 1:
+        return arr
+    # algorithm
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    # return the answer
+    return arr
