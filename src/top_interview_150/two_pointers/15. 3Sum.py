6d4633b5377ad132a27bb4765c08708a82e615c3
Shu-Han Han <dragon1988k@gmail.com>
2025-05-04 21:42:47 +0800

    add 15. 3Sum

--- src/top_interview_150/two_pointers/15. 3Sum.py
+++ src/top_interview_150/two_pointers/15. 3Sum.py
@@ -1,34 +1,37 @@
+from typing import List
+
+
 class Solution:
-    # def threeSum(self, nums: List[int]) -> List[List[int]]:
-    def threeSum(self, nums: list[int]) -> list[list[int]]:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        left, right = 0, len(nums) - 1
-        result_set = set()
+        result = []
 
-        while left + 1 < right:
-            inner_left, inner_right = left + 1, right - 1
-            sum_of_sides = nums[left] + nums[right]
+        for i in range(len(nums) - 2):
+            # Skip duplicate
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-            while inner_left <= inner_right:
-                mid = (inner_left + inner_right) // 2
-                sum_of_triplet = sum_of_sides + nums[mid]
+            left, right = i + 1, len(nums) - 1
 
-                if sum_of_triplet == 0:
-                    result_set.add(tuple(sorted([nums[left], nums[right], nums[mid]])))
-                    break
-                elif sum_of_triplet < 0:
-                    inner_left = mid + 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    # Skip duplicate
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif total > 0:
+                    right -= 1
+
                 else:
-                    inner_right = mid - 1
+                    left += 1
 
-            if sum_of_sides + nums[left + 1] >= 0:
-                right -= 1
-            else:
-                left += 1
-
-        return list(list(triplet) for triplet in result_set)
-
-
-if __name__ == '__main__':
-    s = Solution()
-    s.threeSum([-1, 0, 1, 2, -1, -4])
+        return result
